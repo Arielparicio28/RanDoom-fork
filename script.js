27b1259133874df9ff4a353bc782482c33c6f521
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-05-17 16:12:29 +0200

    sonido burger - aun no suena

---
+++ script.js
@@ -0,0 +1,16 @@
+const img = document.getElementById("menu-icon");
+const sound = document.getElementById("sound");
+
+img.addEventListener("mouseenter", () => {
+    sound.currentTime = 0;
+    sound.play();
+})
+
+const img = document.getElementById("menu-icon");
+const sound = document.getElementById("sound");
+
+img.addEventListener("mouseenter", () => {
+    sound.currentTime = 0;
+    sound.play();
+})
+/* ERROR EN SONIDO */
