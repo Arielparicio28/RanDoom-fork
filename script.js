133cd8e1309df76e1b483a2503cb909cf1d41b08
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-05-17 18:49:12 +0200

    orden menu mobile burguer sound on

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 const img = document.getElementById("menu-icon");
 const sound = document.getElementById("sound");
 
-img.addEventListener("mouseenter", () => {
+img.addEventListener( "mouseenter", () => {
     sound.currentTime = 0;
     sound.play();
 })
-
-const img = document.getElementById("menu-icon");
-const sound = document.getElementById("sound");
-
-img.addEventListener("mouseenter", () => {
-    sound.currentTime = 0;
-    sound.play();
-})
-/* ERROR EN SONIDO */
